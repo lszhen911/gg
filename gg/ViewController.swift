fd6fa21c016f3fbad4b0b708bcc1d8d97b08f7f2
lszhen <lszhen@lszhendemacbook-pro.local>
2017-02-04 15:58:05 +0800

    Initial Commit

---
+++ gg/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gg
+//
+//  Created by lszhen on 2017/2/4.
+//  Copyright © 2017年 lszhen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
