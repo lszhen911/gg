cc57ecbb54b726021454a5e58f25f12715ae604b
lszhen911 <1078169344@qq.com>
2017-02-12 22:58:58 +0800

    Hellotest

---
+++ Hello.java
@@ -0,0 +1,11 @@
+package first;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+		System.out.println("wo de shijie ni ");
+	}
+
+}
